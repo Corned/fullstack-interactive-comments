1fedefada84993214112434ee57809a0c503982c
Oliver K <Corned@users.noreply.github.com>
2024-04-19 10:43:16 +0300

    installed cookieparser

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ import mongoose from "mongoose"
 
 import express from "express"
 import morgan from "morgan"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser"
 
 import commentRouter from "./routers/Comment.js"
 import userRouter from "./routers/User.js"
 import voteRouter from "./routers/Vote.js"
 
-import dotenv from "dotenv"
 dotenv.config()
 
 
@@ -20,11 +21,13 @@ const main = async () => {
 
 main().catch(err => console.log(err))
 
-
 const app = express()
 
+app.use(cookieParser())
 app.use(morgan("tiny"))
+app.use("/user", userRouter)
 app.use("/comment", commentRouter)
+app.use("/vote", voteRouter)
 
 app.get("/", (req, res) => {
   res.send("Hello world!")
