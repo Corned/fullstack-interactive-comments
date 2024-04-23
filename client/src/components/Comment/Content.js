8f07cc5c91798411f589bef4003fb4b4003bcb44
Oliver K <Corned@users.noreply.github.com>
2024-04-23 10:55:16 +0300

    ugly implementation of highlighting @s

--- client/src/components/Comment/Content.js
+++ client/src/components/Comment/Content.js
@@ -1,6 +1,44 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const Content = ({ content }) => {
+
+
+  const r = (/@\w+/gm)
+
+  let match
+  let lastIndex = 0
+  let spans = []
+
+  while (match = r.exec(content)) {
+    const { 0: username, index } = match
+    console.log(username, index);
+
+    let s = content.substring(lastIndex, index)
+    if (s !== "") {
+      console.log("inserting", lastIndex, index, s);
+      spans.push(<span>{ s }</span>)
+    }
+
+    spans.push(<span class="comment__ping">{ content.substr(index, username.length) }</span>)
+    lastIndex = index + username.length
+  }
+
+  spans.push(<span>{ content.substr(lastIndex, content.length) }</span>)
+
   return (
-    <p className="comment__content">{ content }</p>
+    <p className="comment__content">{ spans }</p>
   )
 }
 
